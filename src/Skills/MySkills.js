1f82268e59316d01f14ff2d478c25d5b45294622
AritraMandal <allarounder360@gmail.com>
2025-06-24 00:27:48 +0530

    chnage the skills

--- src/Skills/MySkills.js
+++ src/Skills/MySkills.js
@@ -3,31 +3,31 @@ const Skill = [
         id:1,
         name:"C",
         nameforCss:"c",
-        progress:85
+        progress:"60%"
     },
     {
         id:2,
         name:"C++,STL",
         nameforCss:"cpp",
-        progress:85
+        progress:"60%"
     },
     {
         id:3,
         name:"React.js",
         nameforCss:"react",
-        progress:85
+        progress:"45%"
     },
     {
         id:4,
         name:"Node.js",
         nameforCss:"node",
-        progress:85
+        progress:"40%"
     },
     {
         id:5,
         name:"Express.js",
         nameforCss:"express",
-        progress:85
+        progress:"65%"
     }
 ]
 
