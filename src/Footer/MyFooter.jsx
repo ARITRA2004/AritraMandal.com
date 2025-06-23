1b30b05997645a2afc279047787a6f59d7b0dcc6
AritraMandal <allarounder360@gmail.com>
2025-06-24 00:02:42 +0530

    second commit

--- src/Footer/MyFooter.jsx
+++ src/Footer/MyFooter.jsx
@@ -1,8 +1,22 @@
-import "./Footer.css";
+// import "./Footer.css";
 
 const MyFooter = () => {
   return (
-    <footer class="footer" style={{marginTop:"90px"}}>
+    <footer
+      class="footer"
+      style={{
+        marginTop: "90px",
+        textAlign: "center",
+        padding: "16px",
+        background: "linear-gradient(to right, #141E30, #243B55)",
+        color: "#f1f1f1",
+        fontSize: "16px",
+        borderTop: "2px solid #00f7ff",
+        boxShadow: "0 -2px 12px rgba(0, 0, 0, 0.4)",
+        borderRadius: "8px 8px 0 0",
+        // marginTop: "40px"
+      }}
+    >
       © 2025 Copyright by <span>Aritra Mandal</span>
     </footer>
   );
