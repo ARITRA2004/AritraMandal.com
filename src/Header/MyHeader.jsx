427dd94a547820e775fe1f153d799561df0b88e6
AritraMandal <allarounder360@gmail.com>
2025-06-24 15:33:38 +0530

    style.css of skill changed

--- src/Header/MyHeader.jsx
+++ src/Header/MyHeader.jsx
@@ -3,7 +3,7 @@ const MyHeader = ()=>{
         <header style={{display:"flex",position:"fixed",zIndex:"1",top:0,left:1,right:1, background: "linear-gradient(to right, #0f2027, #203a43, #2c5364)",height:"53px"}}>
             <div style={{display:"flex",width:"100%",justifyContent:"space-between",alignItems:"center",marginInline:"20px"}}>
                 <div className="Image">
-                    <img width="30px" style={{borderRadius:"50%",color:"white"}} src="./logo.png" alt="my-image" />
+                    <img width="30px" style={{borderRadius:"50%",color:"white"}} src="/logo.png" alt="my-image" />
                 </div>
                 <div className="name" style={{fontSize: "16px",fontWeight:"bolder"}}>Aritra Mandal</div>
             </div>
--- src/Skills/Skills.jsx
+++ src/Skills/Skills.jsx
@@ -1,23 +1,28 @@
 import Skill from "./MySkills.js";
-import "./Style.css"
+import "./Style.css";
 
 const Skills = () => {
   return (
-    <div style={{marginTop:"90px"}}>
-      <div style={{marginInline:"100px"}}>
+    <div style={{ marginTop: "90px" }}>
+      <div
+        className="skill-container"
+        // style={{ marginInline: "100px"}}
+      >
         <div>
-          <h2 style={{textAlign:"center",fontSize: "3rem"}}>Skills</h2>
+          <h2 style={{ textAlign: "center", fontSize: "3rem" }}>Skills</h2>
         </div>
-          {
-            Skill.map((value)=>{
-                return(
-                    <div className="skill">
-                    <div className="skill-name">{value.name}</div>
-                    <div className="progress-bar"><div className={`progress ${value.nameforCss}`}>{value.progress}</div></div>
-                  </div>
-                )
-            })
-          }
+        {Skill.map((value) => {
+          return (
+            <div className="skill">
+              <div className="skill-name">{value.name}</div>
+              <div className="progress-bar">
+                <div className={`progress ${value.nameforCss}`}>
+                  {value.progress}
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
